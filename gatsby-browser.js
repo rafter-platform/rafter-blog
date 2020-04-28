493436f66d127d3d70f8ffec66f0816a46fdb45c
Josh Larson <jplhomer@gmail.com>
2020-04-28 12:51:25 -0500

    Whoops

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,2 @@
 const React = require('react')
-const Layout = require('./src/components/Layout').default
 require('./src/styles/global.css')
-
-exports.wrapPageElement = ({ element, props }) => (
-  // props provide same data to Layout as Page element will get
-  // including location, data, etc - you don't need to pass it
-  <Layout {...props}>{element}</Layout>
-)
