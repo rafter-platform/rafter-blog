508b387af70a294f6d1ce5d67d98be1d75023da7
Josh Larson <jplhomer@gmail.com>
2020-04-28 12:45:59 -0500

    More fixes for code highlighting

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,5 +1,6 @@
 const React = require('react')
 const Layout = require('./src/components/Layout').default
+require('./src/styles/global.css')
 
 exports.wrapPageElement = ({ element, props }) => (
   // props provide same data to Layout as Page element will get
